146ccc01e0b90b470408e94bc403f5406e55324a
Justin Wang <wangjpersonal@gmail.com>
2026-05-03 18:34:37 -0700

    repo clean up

--- backend/dist/routes/model.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const model_controller_js_1 = require("../controllers/model.controller.js");
-const router = (0, express_1.Router)();
-router.post('/scan', model_controller_js_1.runScanHandler);
-router.get('/recent-queries', model_controller_js_1.getRecentQueriesHandler);
-router.get('/overall-summary', model_controller_js_1.getOverallProgressHandler);
-exports.default = router;
--- backend/dist/types/models.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supportedModels = void 0;
-exports.supportedModels = ['chatgpt', 'claude', 'gemini'];
